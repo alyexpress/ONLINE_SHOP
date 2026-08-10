a651b263cc25875ff30f08453809f0594e550249
alyexpress <al.ymov@yandex.ru>
2026-08-10 12:43:37 +0300

    Init commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from config import *
+
+
+app = Flask(__name__,
+            template_folder=TEMPLATES_DIR,
+            static_folder=STATIC_DIR)
+
+
+@app.context_processor
+def inject_config():
+    return dict(PROJECT_NAME=PROJECT_NAME,
+                COMPANY_NAME=COMPANY_NAME)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    return render_template('registration.html')
+
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT, debug=DEBUG)
